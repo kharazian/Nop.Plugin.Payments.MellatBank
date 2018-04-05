808e3f26ab60cceae19a3ac7900792149d40f9ff
amir akhavan kharazian <a.akhavan@EDAKHAVAN-0522>
2018-04-05 08:16:48 +0330

    the error has been removed.

---
+++ Nop.Plugin.Payments.MellatBank/DataMappings/TransactionMap.cs
@@ -0,0 +1,17 @@
+﻿using Nop.Plugin.Payments.MellatBank.Domain;
+using System.Data.Entity.ModelConfiguration;
+
+
+namespace Nop.Plugin.Payments.MellatBank.DataMappings
+{
+    public class TransactionMap : EntityTypeConfiguration<Transaction>
+    {
+        public TransactionMap()
+        {
+            ToTable("Transaction_IR");
+
+            HasKey(pt => pt.Id);
+            Property(pt => pt.TransactionId).IsRequired();
+        }
+    }
+}
